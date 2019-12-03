e687718f8bf0df87260faff83954b504ebb236d3
Joshua Smeda <smeda.josh14@gmail.com>
2019-12-03 07:38:36 +0200

    Update run.py

--- run.py
+++ run.py
@@ -7,16 +7,15 @@ source_list = []
 
 for k, v in configuration.source_details.items():
     if v == True:
-     #   print("%s is selected to be ingested" % (k))
+        print("%s is selected to be ingested" % (k))
         source_list.append(k)
-    elif v == False:
+    else:
         print("%s is selected to not be ingested" % (k))
 
 print("\nStarting main process shortly in 10 seconds\n")
-time.sleep(3)
+time.sleep(10)
 
 
 for i in source_list:
-    print("Starting thread for %s" % (i))
     p = multiprocessing.Process(target=lambda: __import__(i))
     p.start()
